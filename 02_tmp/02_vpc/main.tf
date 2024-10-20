86a6f96bf15dad3588943bfd2e9662f98339f5a5
max <81824845+maxxam123@users.noreply.github.com>
2024-10-20 08:18:01 -0700

    Update main.tf

--- 02_tmp/02_vpc/main.tf
+++ 02_tmp/02_vpc/main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "RESOURCE" {
+resource "aws_vpc" "RESOURCE_VPC" {
   cidr_block = "10.0.0.0/16"
 
   enable_dns_hostnames = true
@@ -6,5 +6,35 @@ resource "aws_vpc" "RESOURCE" {
   
   tags = {
     Name = var.vpc
+  }
+}
+
+resource "aws_subnet" "RESOURCE_PUB_SUBNET" {
+  vpc_id     = aws_vpc.RESOURCE_VPC.id
+  count = var.count
+  cidr_block = cidrsubnet(aws_vpc.RESOURCE_VPC.cidr_block, 8, count.index + 1)
+  availability_zone = "eu-central-1a"
+
+  tags = {
+    Name = "Public Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "RESOURCE_PRI_SUBNET" {
+  vpc_id     = aws_vpc.RESOURCE_VPC.id
+  count = var.count
+  cidr_block = cidrsubnet(aws_vpc.RESOURCE_VPC, 8, count.index + 3 )
+  availability_zone = "eu-central-1a"
+
+  tags = {
+    Name = "Private Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "RESOURCE_IGW" {
+  vpc_id = aws_vpc.RESOURCE_VPC.id
+
+  tags = {
+    Name = "Internet Gateway"
   }
 }
