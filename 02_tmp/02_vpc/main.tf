babdfd88b6e405c6ec2805c6178f79275b2d9678
max <81824845+maxxam123@users.noreply.github.com>
2024-10-20 08:50:21 -0700

    Update main.tf

--- 02_tmp/02_vpc/main.tf
+++ 02_tmp/02_vpc/main.tf
@@ -8,33 +8,3 @@ resource "aws_vpc" "RESOURCE_VPC" {
     Name = var.vpc
   }
 }
-
-resource "aws_subnet" "RESOURCE_PUB_SUBNET" {
-  vpc_id     = aws_vpc.RESOURCE_VPC.id
-  count = var.number
-  cidr_block = cidrsubnet(aws_vpc.RESOURCE_VPC.cidr_block, 8, count.index + 1)
-  availability_zone = "eu-central-1a"
-
-  tags = {
-    Name = "Public Subnet ${count.index + 1}"
-  }
-}
-
-resource "aws_subnet" "RESOURCE_PRI_SUBNET" {
-  vpc_id     = aws_vpc.RESOURCE_VPC.id
-  count = var.number
-  cidr_block = cidrsubnet(aws_vpc.RESOURCE_VPC.cidr_block, 8, count.index + 3 )
-  availability_zone = "eu-central-1a"
-
-  tags = {
-    Name = "Private Subnet ${count.index + 1}"
-  }
-}
-
-resource "aws_internet_gateway" "RESOURCE_IGW" {
-  vpc_id = aws_vpc.RESOURCE_VPC.id
-
-  tags = {
-    Name = "Internet Gateway"
-  }
-}
