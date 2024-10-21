be2f0ea036c3ba29dee91485c3a8d5bcc58df521
max <81824845+maxxam123@users.noreply.github.com>
2024-10-21 02:56:55 -0700

    Update main.tf

--- 02_tmp/02_vpc/main.tf
+++ 02_tmp/02_vpc/main.tf
@@ -6,3 +6,24 @@ resource "aws_vpc" "vpc_multi" {
     Name = "${var.name}-${count.index+1}"
   }
 }
+
+resource "aws_internet_gateway" "vpc_multi_igw" {
+  vpc_id = aws_vpc.vpc_multi.id
+
+  tags = {
+    Name = "Internet Gateway ${var.name}"
+  }
+}
+
+resource "aws_route_table" "vpc_multi_rt" {
+  vpc_id = aws_vpc.vpc_multi.id
+
+  route {
+    cidr_block = "0.0.0..0/0"
+    gateway_id = aws_internet_gateway.vpc_multi_igw.id
+  }
+
+  tags = {
+    Name = "Route Table Public ${var.name}"
+  }
+}
