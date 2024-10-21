87f9a8218caa1b1934c22be598a919604361c743
max <81824845+maxxam123@users.noreply.github.com>
2024-10-21 05:09:42 -0700

    Update main.tf

--- 02_tmp/03_subnet_pub/main.tf
+++ 02_tmp/03_subnet_pub/main.tf
@@ -1,47 +1,21 @@
 
-data "aws_vpc" "vpc_data_pri" {
+data "aws_vpc" "vpc_data" {
   id = var.vpc_id
 }
 
-resource "aws_subnet" "pri_sn" {
-  vpc_id     = data.aws_vpc.vpc_data_pri.id
-  count = var.number2
-  cidr_block = cidrsubnet(data.aws_vpc.vpc_data_pri.cidr_block, 8, count.index + var.subnet)
+resource "aws_subnet" "pub_sn_multi" {
+  vpc_id     = data.aws_vpc.vpc_data.id
+  count = var.number
+  cidr_block = cidrsubnet(data.aws_vpc.vpc_data.cidr_block, 8, count.index + var.subnet)
   availability_zone = var.az
 
   tags = {
-    Name = " private ${var.name}-${count.index+1}"
+    Name = "public ${var.name}-${count.index+1}"
   }
 }
 
-resource "aws_eip" "eip_pri" {
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "nat_pri" {
-  subnet_id     = element(aws_subnet.pri_sn[*].id, 0)
-  allocation_id = aws_eip.eip_pri.id
-
-  tags = {
-    Name = "Nat Gateway ${var.name}"  
-  }
-}
-
-resource "aws_route_table" "rt_pri" {
-  vpc_id = data.aws_vpc.vpc_data_pri.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_pri.id
-  }
-
-  tags = {
-    Name = "Route Table Private ${var.name}"
-  }
-}
-
-resource "aws_route_table_association" "autoscaling_01_ass_private" {
-  route_table_id = aws_route_table.rt_pri.id
-  count = var.number2
-  subnet_id      = element(aws_subnet.pri_sn[*].id, count.index)
+resource "aws_route_table_association" "autoscaling_01_ass_public" {
+  route_table_id = var.route_table
+  count = var.number
+  subnet_id      = element(aws_subnet.pub_sn_multi[*].id, count.index)
 }
