c1406cfd8b9e0de0a07d3d4b409d0de01bb56de1
max <81824845+maxxam123@users.noreply.github.com>
2024-10-21 05:10:05 -0700

    Create variables.tf

---
+++ 02_tmp/03_subnet_pub/variables.tf
@@ -0,0 +1,35 @@
+variable "vpc_id" {
+  description = "vpc_id"
+  type = string
+  default = ""
+}
+
+variable "number" {
+  description = "number"
+  type = string
+  default = "1"
+}
+
+variable "subnet" {
+  description = "subnet"
+  type = string
+  default = "4"
+}
+
+variable "az" {
+  description = "az"
+  type = string
+  default = "eu-central-1a"
+}
+
+variable "route_table" {
+  description = "route_table"
+  type = string
+  default = ""
+}
+
+variable "name" {
+  description = "name"
+  type = string
+  default = "subnet_default"
+}
