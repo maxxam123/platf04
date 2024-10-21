137da0bba485412e0bafcc0d2f69b21b2bb4f233
max <81824845+maxxam123@users.noreply.github.com>
2024-10-21 02:04:19 -0700

    Update main.tf

--- 02_tmp/02_vpc/main.tf
+++ 02_tmp/02_vpc/main.tf
@@ -1,40 +1,8 @@
-resource "aws_vpc" "RESOURCE_VPC" {
-  cidr_block = "10.0.0.0/16"
 
-  enable_dns_hostnames = true
-  enable_dns_support = true
-  
-  tags = {
-    Name = var.vpc
-  }
-}
-
-resource "aws_subnet" "RESOURCE_PUB_SUBNET" {
-  vpc_id     = aws_vpc.RESOURCE_VPC.id
+resource "aws_vpc" "vpc_multi" {
   count = var.number
-  cidr_block = cidrsubnet(aws_vpc.RESOURCE_VPC.cidr_block, 8, count.index + 1)
-  availability_zone = "eu-central-1a"
-
+  cidr_block = "1${count.index+1}.0.0.0/16"
   tags = {
-    Name = "Public Subnet ${count.index + 1}"
-  }
-}
-
-resource "aws_subnet" "RESOURCE_PRI_SUBNET" {
-  vpc_id     = aws_vpc.RESOURCE_VPC.id
-  count = var.number
-  cidr_block = cidrsubnet(aws_vpc.RESOURCE_VPC.cidr_block, 8, count.index + 3 )
-  availability_zone = "eu-central-1a"
-
-  tags = {
-    Name = "Private Subnet ${count.index + 1}"
-  }
-}
-
-resource "aws_internet_gateway" "RESOURCE_IGW" {
-  vpc_id = aws_vpc.RESOURCE_VPC.id
-
-  tags = {
-    Name = "Internet Gateway"
+    Name = "${var.name}-${count.index+1}"
   }
 }
