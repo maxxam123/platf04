6e65114b996c4de5e02d864d4120820d4896d525
max <81824845+maxxam123@users.noreply.github.com>
2024-10-24 08:13:37 -0700

    Delete 01_infra/10_eks/aws/01_aws_eks directory

--- 01_infra/10_eks/aws/01_aws_eks/scripts/script.sh
+++
@@ -1,6 +0,0 @@
-cp ../../../../02_tmp/10_eks/aws/08_autoscaler.tf .
-cp ../../../../02_tmp/10_eks/aws/09_efs.tf .
-cp ../../../../02_tmp/10_eks/aws/10_secdriver.tf .
-cp ../../../../02_tmp/SERVICE/PROVIDER/01_variables.tf .
-cp ../../../../02_tmp/SERVICE/PROVIDER/03_main.tf .
-cp ../../../../03_pro/PROVIDER/02_provider.tf .
