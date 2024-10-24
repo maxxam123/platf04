621cd8026c26da2d782601702d61e0189eccc867
max <81824845+maxxam123@users.noreply.github.com>
2024-10-24 00:37:26 -0700

    Update 02_provider.tf

--- 03_pro/aws/02_provider.tf
+++ 03_pro/aws/02_provider.tf
@@ -1,1 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.72.1"
+    }
+  }
+}
 
+provider "aws" {
+#  region = "eu-central-1"
+  region = var.region
+}
