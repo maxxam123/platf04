a038e55b9f472b0dbe2a7c6ce02ef459624c1b1f
maxxam123 <hennighausenmax@gmail.com>
2024-10-24 15:19:00 +0000

    Update from https://github.com/maxxam123/platform02/commit/eff20ae55ed6679c2509bc0fedb28196f463f98e

---
+++ 01_infra/10_eks/aws/01_aws_eks/scripts/script.sh
@@ -0,0 +1,6 @@
+cp ../../../../02_tmp/10_eks/aws/08_autoscaler.tf .
+cp ../../../../02_tmp/10_eks/aws/09_efs.tf .
+cp ../../../../02_tmp/10_eks/aws/10_secdriver.tf .
+cp ../../../../02_tmp/10_eks/aws/01_variables.tf .
+cp ../../../../02_tmp/10_eks/aws/03_main.tf .
+cp ../../../../03_pro/aws/02_provider.tf .
