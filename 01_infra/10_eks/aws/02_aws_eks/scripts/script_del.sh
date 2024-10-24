ffb6b5d2aee6b296cf7f59a08546a4f0932da983
maxxam123 <hennighausenmax@gmail.com>
2024-10-24 14:38:16 +0000

    Update from https://github.com/maxxam123/platform02/commit/854ccbedf0d5fac5cd6dce31bc0c8af32d13d249

--- 01_infra/10_eks/aws/02_aws_eks/scripts/script_del.sh
+++ 01_infra/10_eks/aws/02_aws_eks/scripts/script_del.sh
@@ -1,3 +1,6 @@
 cp ../../../../02_tmp/10_eks/aws/08_autoscaler.tf .
 cp ../../../../02_tmp/10_eks/aws/09_efs.tf .
 cp ../../../../02_tmp/10_eks/aws/10_secdriver.tf .
+cp ../../../../02_tmp/10_eks/aws/08_autoscaler.tf .
+cp ../../../../02_tmp/10_eks/aws/09_efs.tf .
+cp ../../../../02_tmp/10_eks/aws/10_secdriver.tf .
