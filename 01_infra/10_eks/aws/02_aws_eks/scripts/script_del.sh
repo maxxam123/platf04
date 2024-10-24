b482f3743e03f1ea8f1017c0b322f7cb821c5a43
maxxam123 <hennighausenmax@gmail.com>
2024-10-24 14:38:32 +0000

    Update from https://github.com/maxxam123/platf04/commit/ffb6b5d2aee6b296cf7f59a08546a4f0932da983

--- 01_infra/10_eks/aws/02_aws_eks/scripts/script_del.sh
+++
@@ -1,6 +0,0 @@
-cp ../../../../02_tmp/10_eks/aws/08_autoscaler.tf .
-cp ../../../../02_tmp/10_eks/aws/09_efs.tf .
-cp ../../../../02_tmp/10_eks/aws/10_secdriver.tf .
-cp ../../../../02_tmp/10_eks/aws/08_autoscaler.tf .
-cp ../../../../02_tmp/10_eks/aws/09_efs.tf .
-cp ../../../../02_tmp/10_eks/aws/10_secdriver.tf .
