2a4c9ae12440c05edd7cdae660deb2364843fe30
max <81824845+maxxam123@users.noreply.github.com>
2024-10-24 00:35:23 -0700

    Update 02_provider.tf

--- 03_pro/gcp/02_provider.tf
+++ 03_pro/gcp/02_provider.tf
@@ -1,1 +1,15 @@
+provider "google" {
+  project     = "my-project-id"
+  region      = "us-central1"
+  project     = "my-project-id"
+  region      = "us-central1"
+}
 
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
